e0f08184ee160306b0331e82c3db77f3c5fc46d6
Azamat Zulpykhar <augustintherome@gmail.com>
2022-08-23 08:53:25 +0600

    feat: added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+const { configure, presets } = require("eslint-kit");
+
+module.exports = configure({
+  presets: [
+    presets.node(),
+    presets.typescript(),
+    presets.react(),
+    presets.nextJs(),
+    presets.prettier(),
+    presets.imports(),
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-
-
 export default function Index() {
-  return (
-    <>Index</>
-  )
+  return <>Index</>;
 }
