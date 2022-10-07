d968e3642b9c63d73fc17c74ffbb72dfe84dc318
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-07 20:05:35 +0600

    feat: added tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "app/core/tailwind.css";
 import { type AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
 export default function Index() {
-  return <Link href={{ pathname: "/" }}>Index</Link>;
+  return (
+    <Link href={{ pathname: "/" }} passHref>
+      <a href="replaced" className="text-3xl font-bold underline">
+        Index
+      </a>
+    </Link>
+  );
 }
