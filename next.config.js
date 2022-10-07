0fba5a1466345558fd34ead316c90a12a6f2f97e
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-07 19:42:18 +0600

    feat: added nextjs-routes for typesafe routing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withRoutes = require("nextjs-routes/config")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withRoutes(nextConfig);
